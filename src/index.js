d4e4bcd1104e5723435d896008157ca463dc127d
Snezhana <snowwe25@rambler.ru>
2018-09-24 15:59:39 +0300

    version_1

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.arr = [];
+        this.DefaultComparator = (a, b) => a - b;
+        this.compareFunction = this.DefaultComparator;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.arr.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.arr.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.arr.slice();
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        // your implementation
+        let i = 0;
+        let arr = [];
+        for (i = 0; i < indices.length; i++) {
+            arr.push(this.arr[indices[i]]);
+        }
+        arr.sort(this.compareFunction);
+        indices.sort(function (x, y) {
+            return x - y
+        });
+        for (i = 0; i < indices.length; i++) {
+            this.arr[indices[i]] = arr[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        // your implementation
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
